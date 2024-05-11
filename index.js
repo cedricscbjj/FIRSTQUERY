7a1f574e146e98324195854277752cfd9452d6f6
cedricscbjj <63507246+cedricscbjj@users.noreply.github.com>
2024-05-11 21:31:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 
-
 const express = require('express');
 const app = express();
 const PORT = 2111;
@@ -12,13 +11,13 @@ app.set('views', __dirname + '/app/views');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-const db = new sqlite3.Database('/home/student/testDB.db');
+const db = new sqlite3.Database('/home/student/islamDB.db');
 
 app.post('/submit', (req, res) => {
     const { input1, input2, input3, input4 } = req.body;
     //  ca c'est pour Insérer les données dans la base de données
     
-    db.run(`INSERT INTO texteislam (type_de_livre, numero_du_verset, sujet_du_verset, auteur) VALUES (?, ?, ?, ?)`, [input1, input2, input3, input4], function(err) {
+    db.run(`INSERT INTO islamDB (type_de_livre, numero_du_verset, sujet_du_verset, auteur) VALUES (?, ?, ?, ?)`, [input1, input2, input3, input4], function(err) {
         if (err) {
             return console.error(err.message);
         }
@@ -31,7 +30,7 @@ app.post('/submit', (req, res) => {
 
 app.post('/search', (req, res) => {
     const userInput = req.body.input5;
-    const sql = `SELECT * FROM texteislam WHERE sujet_du_verset LIKE '%${userInput}%'`;
+    const sql = `SELECT * FROM islamDB WHERE sujet_du_verset LIKE '%${userInput}%'`;
   
     db.all(sql, [], (err, rows) => {
       if (err) {
@@ -43,7 +42,18 @@ app.post('/search', (req, res) => {
   
 
 
+  app.post('/delete', (req, res) => {
+    const id = req.body.input6; // Récupérer l'ID depuis le formulaire
+    const sql = 'DELETE FROM islamDB WHERE islam_id = ?';
 
+    db.run(sql, id, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`Ligne avec l'ID ${id} supprimée de la base de données.`);
+        res.redirect('/');
+    });
+});
 
 
   app.get('/', (req, res) => {
@@ -54,10 +64,3 @@ app.post('/search', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Serveur sur le port ${PORT}`);
 });
-
-
-
-
-
-
-
